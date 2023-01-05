3e8e7c005b336400ff9e988516c2dfbea5c54a78
JoanneUTS <joanne.pulcina@student.uts.edu.au>
2023-01-05 21:39:55 +1100

    created hangman class with constructor class, display method and letter check method.

---
+++ Hangman/src/Hangman/HangmanWord.java
@@ -0,0 +1,43 @@
+package Hangman;
+
+import java.util.ArrayList;
+
+public class HangmanWord {
+	private String selectedWord = null;
+	//displayWord: An array list of the selected word as displayed to player.Underscores represent the unidentified letters. Identified letters will appear in array. 
+	private ArrayList<Character> displayWord= new ArrayList<Character>();   
+	
+	public HangmanWord(String word) {
+		this.selectedWord = word; 
+		this.displayWord = initialiseDisplayWord(word);
+	}
+	
+	//Upon instansiation an array list of underscores the length of the selected word is created.  
+	private ArrayList<Character> initialiseDisplayWord(String word){
+		ArrayList<Character> displayWord = new ArrayList<Character>();
+		for(int i=0;i<word.length();i++) {
+			displayWord.add('_');
+		}
+		return displayWord;
+	}
+	
+	/*Prints to console the progress of the guessed word */
+	public String printDisplayWord(){
+		String displayString = "";
+		for(int i=0;i<this.displayWord.size() ;i++) {
+			displayString += this.displayWord.get(i).toString()+" ";
+		}
+		return displayString;
+	}
+	
+	/*Function checks if letter argument is in the selected word. If it is it changes the displayed
+	word so that the correctly chosen letter appears*/
+	public void checkLetter(char letter) {
+		for(int i=0; i<this.selectedWord.length(); i++) {
+			if(selectedWord.charAt(i)==letter) {
+				displayWord.set(i,letter);
+			}
+		}
+	}
+
+}
--- Hangman/src/Hangman/Main.java
+++ Hangman/src/Hangman/Main.java
@@ -1,10 +1,29 @@
 package Hangman;
 
+import java.util.Scanner; 
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean gameFinished=false; 
+		
+		//Select random word using random word selector class
+		//Create instance of WordProgress class 
+		HangmanWord word = new HangmanWord("hello");
+		System.out.println(word.printDisplayWord());
+		
+		//Prompt user to enter letter
+		System.out.println("Please enter a letter:");
+		Scanner keyboard = new Scanner(System.in);
+		char input1 = keyboard.next().charAt(0);
+		System.out.printf("You selected %s%n",input1);
 
+		//Check if letter is in word 
+		word.checkLetter(input1);
+		System.out.println(word.printDisplayWord());
+		
+		//Hangman image 
+		
 	}
 
 }
